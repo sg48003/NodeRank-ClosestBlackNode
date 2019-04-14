03eec4c15a43ae022f0d0ee6c5aa106bdabfded9
Sebastian Glad <sg48003@fer.hr>
2019-04-14 19:10:01 +0200

    Add project files.

---
+++ ClosestBlackNode/ClosestBlackNode.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClosestBlackNode
+{
+    class ClosestBlackNode
+    {
+        public class Node
+        {
+            public int Number;
+            public Color Color;
+            public Boolean Visited;
+            public Node ClosestBlackNode;
+            public int DistanceToClosestBlackNode;
+            public List<Node> ConnectedNodes = new List<Node>();
+
+        }
+
+        public enum Color
+        {
+            White,
+            Black
+        }
+
+        static void Main(string[] args)
+        {
+
+            #region Testing
+
+            //int numberOfNodes;
+            //double accidentalWalkerProbability;
+            //var listOfNodes = new List<Node>();
+
+            //const Int32 BufferSize = 128;
+            //using (var fileStream = File.OpenRead("S:\\Projekti\\NodeRank_ClosestBlackNode\\NodeRank_ClosestBlackNode\\bin\\Debug\\R.in"))
+            //using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            //{
+
+                //var line1 = Console.ReadLine().Split();
+                //var numberOfNodes = Convert.ToInt32(line1[0]);
+                //var numberOfEdges = Convert.ToInt32(line1[1]);
+
+                //var listOfNodes = new List<Node>();
+                //var listOfBlackNodes = new List<Node>();
+                //for (var i = 0; i < numberOfNodes; i++)
+                //{
+                //    var color = Convert.ToInt32(Console.ReadLine());
+                //    var node = new Node { Number = i };
+                //    if (color == (int)Color.Black)
+                //    {
+                //        node.Color = Color.Black;
+                //        node.DistanceToClosestBlackNode = 0;
+                //        node.ClosestBlackNode = node;
+                //        listOfBlackNodes.Add(node);
+                //    }
+                //    else
+                //    {
+                //        node.Color = Color.White;
+                //        node.DistanceToClosestBlackNode = int.MaxValue;
+                //    }
+                //    listOfNodes.Add(node);
+                //}
+
+                //for (var i = 0; i < numberOfEdges; i++)
+                //{
+                //    var listOfEdges = Console.ReadLine().Split().Select(int.Parse).ToList();
+                //    listOfNodes[listOfEdges[0]].ConnectedNodes.Add(listOfNodes[listOfEdges[1]]);
+                //    listOfNodes[listOfEdges[1]].ConnectedNodes.Add(listOfNodes[listOfEdges[0]]);
+                //}
+
+            //    }
+
+            //}
+
+            #endregion
+
+            #region Initialization
+
+            var line1 = Console.ReadLine().Split();
+            var numberOfNodes = Convert.ToInt32(line1[0]);
+            var numberOfEdges = Convert.ToInt32(line1[1]);
+
+            var listOfNodes = new List<Node>();
+            var listOfBlackNodes = new List<Node>();
+            for (var i = 0; i < numberOfNodes; i++)
+            {
+                var color = Convert.ToInt32(Console.ReadLine());
+                var node = new Node { Number = i };
+                if (color == (int)Color.Black)
+                {
+                    node.Color = Color.Black;
+                    node.DistanceToClosestBlackNode = 0;
+                    node.ClosestBlackNode = node;
+                    listOfBlackNodes.Add(node);
+                }
+                else
+                {
+                    node.Color = Color.White;
+                    node.DistanceToClosestBlackNode = int.MaxValue;
+                }
+                listOfNodes.Add(node);
+            }
+
+            for (var i = 0; i < numberOfEdges; i++)
+            {
+                var listOfEdges = Console.ReadLine().Split().Select(int.Parse).ToList();
+                listOfNodes[listOfEdges[0]].ConnectedNodes.Add(listOfNodes[listOfEdges[1]]);
+                listOfNodes[listOfEdges[1]].ConnectedNodes.Add(listOfNodes[listOfEdges[0]]);
+            }
+
+            #endregion
+
+            #region Closest Black Node
+
+            while (listOfBlackNodes.Count > 0)
+            {
+                var node = listOfBlackNodes[0];
+                node.Visited = true;
+                foreach (var connectedNode in node.ConnectedNodes)
+                {
+                    if (connectedNode.Color == Color.White && connectedNode.Visited == false)
+                    {
+                        connectedNode.Visited = true;
+                        listOfBlackNodes.Add(connectedNode);
+                        if (connectedNode.DistanceToClosestBlackNode >= node.DistanceToClosestBlackNode)
+                        {
+                            listOfNodes[connectedNode.Number].DistanceToClosestBlackNode = node.DistanceToClosestBlackNode + 1;
+                            listOfNodes[connectedNode.Number].ClosestBlackNode = node.ClosestBlackNode;
+                        }
+                    }
+                }
+                listOfBlackNodes.Remove(node);
+
+            }
+
+            #endregion
+
+            #region Output
+
+            foreach (var node in listOfNodes)
+            {
+                if (node.DistanceToClosestBlackNode == int.MaxValue)
+                {
+                    Console.WriteLine("-1 -1");
+                }
+                else
+                {
+                    Console.WriteLine(node.ClosestBlackNode.Number + " " + node.DistanceToClosestBlackNode);
+                }
+            }
+
+            #endregion
+
+        }
+    }
+}
---
+++ NodeRank_ClosestBlackNode/NodeRank.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NodeRank_ClosestBlackNode
+{
+    class NodeRank
+    {
+        public class Node
+        {
+            public double Rank;
+            public List<int> ListOfOutGoingPointers = new List<int>();
+            public List<int> ListOfInGoingPointers = new List<int>();
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            #region Testing
+
+            //int numberOfNodes;
+            //double accidentalWalkerProbability;
+            //var listOfNodes = new List<Node>();
+
+            //const Int32 BufferSize = 128;
+            //using (var fileStream = File.OpenRead("S:\\Projekti\\NodeRank_ClosestBlackNode\\NodeRank_ClosestBlackNode\\bin\\Debug\\R.in"))
+            //using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            //{
+            //    var line1 = streamReader.ReadLine().Split();
+            //    numberOfNodes = Convert.ToInt32(line1[0]);
+            //    accidentalWalkerProbability = Convert.ToDouble(line1[1]);
+
+            //    for (var i = 0; i < numberOfNodes; i++)
+            //    {
+            //        var node = new Node
+            //        {
+            //            Rank = (float) 1 / numberOfNodes
+            //        };
+            //        listOfNodes.Add(node);
+            //    }
+
+            //    for (var i = 0; i < numberOfNodes; i++)
+            //    {
+            //        var listOfEdges = streamReader.ReadLine().Split().Select(int.Parse).ToList();
+
+            //        listOfNodes[i].NumberOfOutGoingPointers = listOfEdges.Count;
+            //        foreach (var connectedNode in listOfEdges)
+            //        {
+            //            listOfNodes[connectedNode].ListOfInGoingPointers.Add(i);
+            //        }
+
+            //    }
+
+            //}
+
+            #endregion
+
+            #region Initialization
+
+            var line1 = Console.ReadLine().Split();
+            var numberOfNodes = Convert.ToInt32(line1[0]);
+            var accidentalWalkerProbability = Convert.ToDouble(line1[1]);
+
+            var listOfNodes = new List<Node>();
+            //postavljanje inicijalne vrijednosti ranka svih čvorova
+            for (var i = 0; i < numberOfNodes; i++)
+            {
+                var node = new Node
+                {
+                    Rank = (double) 1 / numberOfNodes
+                };
+                listOfNodes.Add(node);
+            }
+
+            for (var i = 0; i < numberOfNodes; i++)
+            {
+                var listOfEdges = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+                listOfNodes[i].ListOfOutGoingPointers = listOfEdges;
+                foreach (var connectedNode in listOfEdges)
+                {
+                    listOfNodes[connectedNode].ListOfInGoingPointers.Add(i);
+                }
+
+            }
+
+            #endregion
+
+            #region PageRank
+
+            //može ići do 100 iteracija
+            var rank = new double[100 + 1,numberOfNodes];
+            //inicijalna vrijednost nulte iteracije svih čvororva
+            for (var i = 0; i < numberOfNodes; i++)
+            {
+                rank[0, i] = (double) 1 / numberOfNodes;
+            }
+
+            for (var i = 0; i < 100; i++)
+            {
+                double S = 0;
+                for (var j = 0; j < numberOfNodes; j++)
+                {
+                    foreach (var pointer in listOfNodes[j].ListOfInGoingPointers)
+                    {
+                        rank[i + 1,j] += accidentalWalkerProbability * rank[i,pointer] / listOfNodes[pointer].ListOfOutGoingPointers.Count;
+                    }
+                    S += rank[i + 1, j];
+                }
+
+                for (var j = 0; j < numberOfNodes; j++)
+                {
+                    rank[i + 1, j] += (1 - S) / numberOfNodes;
+                }
+            }
+
+            #endregion
+
+            #region Output
+
+            var numberOfQueries = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; i < numberOfQueries; i++)
+            {
+                var queryLine = Console.ReadLine().Split().Select(int.Parse).ToList();
+                Console.WriteLine(string.Format("{0:0.0000000000}", rank[queryLine[1], queryLine[0]]));
+            }
+
+            #endregion
+
+        }
+    }
+}
